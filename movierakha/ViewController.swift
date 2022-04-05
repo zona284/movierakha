21c318dc3db9a35d6ec46b65b8ee9fc985caf834
rakhalima <86281014+rakhalima@users.noreply.github.com>
2022-04-05 16:29:52 +0700

    Initial Commit

---
+++ movierakha/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  movierakha
+//
+//  Created by LIMA 1 on 05/04/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
