befe8fa704c3246feca34b56f3477c21a5383c7f
zona284 <rakha.mauludi@gmail.com>
2022-04-06 21:47:07 +0700

    ui improvements and unit test parse json data

--- movierakhaTests/movierakhaTests.swift
+++ movierakhaTests/movierakhaTests.swift
@@ -5,32 +5,82 @@
 //  Created by LIMA 1 on 05/04/22.
 //
 
+import Foundation
 import XCTest
 @testable import movierakha
 
 class movierakhaTests: XCTestCase {
 
-    override func setUpWithError() throws {
-        // Put setup code here. This method is called before the invocation of each test method in the class.
-    }
-
-    override func tearDownWithError() throws {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
-    }
-
-    func testExample() throws {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
-        // Any test you write for XCTest can be annotated as throws and async.
-        // Mark your test throws to produce an unexpected failure when your test encounters an uncaught error.
-        // Mark your test async to allow awaiting for asynchronous code to complete. Check the results with assertions afterwards.
-    }
-
-    func testPerformanceExample() throws {
-        // This is an example of a performance test case.
-        self.measure {
-            // Put the code you want to measure the time of here.
+    func testParseJson_Movie() {
+        let json = """
+        {
+            "page": 1,
+            "results": [
+                {
+                    "adult": false,
+                    "backdrop_path": "/iQFcwSGbZXMkeyKrxbPnwnRo5fl.jpg",
+                    "genre_ids": [
+                        28,
+                        12,
+                        878
+                    ],
+                    "id": 634649,
+                    "original_language": "en",
+                    "original_title": "Spider-Man: No Way Home",
+                    "overview": "Peter Parker is unmasked and no longer able to separate his normal life from the high-stakes of being a super-hero. When he asks for help from Doctor Strange the stakes become even more dangerous, forcing him to discover what it truly means to be Spider-Man.",
+                    "popularity": 6064.019,
+                    "poster_path": "/1g0dhYtq4irTY1GPXvft6k4YLjm.jpg",
+                    "release_date": "2021-12-15",
+                    "title": "Spider-Man: No Way Home",
+                    "video": false,
+                    "vote_average": 8.2,
+                    "vote_count": 11022
+                }
+            ],
+            "total_pages": 33023,
+            "total_results": 660442
         }
+        """.data(using: .utf8)!
+        
+        let decoder = JSONDecoder()
+        let resultData = try? decoder.decode(MoviesResult.self, from: json)
+        
+        XCTAssertNotNil(resultData)
+        XCTAssertEqual(resultData?.results?.first?.id, 634649)
+    }
+    
+    func testParseJson_Review() {
+        let json = """
+        {
+            "id": 634649,
+            "page": 1,
+            "results": [
+                {
+                    "author": "garethmb",
+                    "author_details": {
+                        "name": "",
+                        "username": "garethmb",
+                        "avatar_path": "/https://www.gravatar.com/avatar/3593437cbd05cebe0a4ee753965a8ad1.jpg",
+                        "rating": 10.0
+                    },
+                    "content": "Life for Peter Parker",
+                    "created_at": "2021-12-15T15:35:00.071Z",
+                    "id": "61ba0b24d1444300413e2cbe",
+                    "updated_at": "2021-12-15T15:35:00.071Z",
+                    "url": "https://www.themoviedb.org/review/61ba0b24d1444300413e2cbe"
+                }
+            ],
+            "total_pages": 1,
+            "total_results": 8
+        }
+        """.data(using: .utf8)!
+        
+        let decoder = JSONDecoder()
+        let resultData = try? decoder.decode(ReviewsResult.self, from: json)
+        
+        XCTAssertNotNil(resultData)
+        XCTAssertEqual(resultData?.id, 634649)
+        XCTAssertEqual(resultData?.results?.first?.author, "garethmb")
     }
 
 }
